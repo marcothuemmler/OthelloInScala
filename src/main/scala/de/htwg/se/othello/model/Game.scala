72b7e1d805602f3d1cb48d385928d0f7bfc9a0fa
marcothuemmler <marco.thuemmler@me.com>
2019-04-05 14:49:07 +0200

    Started to clean up flip method

--- src/main/scala/de/htwg/se/othello/model/Game.scala
+++ src/main/scala/de/htwg/se/othello/model/Game.scala
@@ -7,22 +7,17 @@ case class Game(board: Board) {
   def update(): Unit = println(board)
 
   def flip(n: (Int, Int), o: (Int, Int), value: Int): Unit = {
-    if (n._1 == o._1 && n._2 < o._2) { //up
-      for (i <- n._2 until o._2) {
+    val x = if (n._2 < o._2) 1 else -1
+    val y = if (n._1 < o._1) 1 else -1
+    if (n._1 == o._1 && n._2 != o._2) { // vertical
+      for (i <- n._2 until o._2 by x) {
         flip(n._1, i, value)
       }
-    } else if (n._1 < o._1 && n._2 == o._2) { // left
-      for (i <- n._1 until o._1) {
+    } else if (n._1 != o._1 && n._2 == o._2) { // horizontal
+      for (i <- n._1 until o._1 by y) {
         flip(i, n._2, value)
       }
-    } else if (n._1 > o._1 && n._2 == o._2) { // right
-      for (i <- o._1 to n._1) {
-        flip(i, n._2, value)
-      }
-    } else if (n._1 == o._1 && n._2 > o._2) { // down
-      for (i <- o._2 to n._2) {
-        flip(o._1, i, value)
-      }
+
     } else if (n._1 < o._1 && n._2 < o._2) { // up left
       for (i <- n._1 to o._1; j <- n._2 to o._2 if i - n._1 == j - n._2) {
         flip(i, j, value)
