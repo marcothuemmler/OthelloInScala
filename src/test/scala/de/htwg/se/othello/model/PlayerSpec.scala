78225b1563b5f8f1e47395b75b10e62ef440c3cd
marcothuemmler <marco.thuemmler@me.com>
2019-04-09 00:22:40 +0200

    Reached code coverage of 100% in Player class

--- src/test/scala/de/htwg/se/othello/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/othello/model/PlayerSpec.scala
@@ -16,7 +16,6 @@ class PlayerSpec extends WordSpec with Matchers {
       player.setByOpp(3, 4) should be (false)
     }
   }
-
   "setByPl" should {
     "Be false if set by opponent" in {
       player.setByPl(4, 4) should be (false)
@@ -34,8 +33,8 @@ class PlayerSpec extends WordSpec with Matchers {
     }
   }
   "highlight " should {
-    player.highlight()
     "set highlighted cells to" in {
+      player.highlight()
       player.board.field(2)(3).toString should be ("x ")
     }
   }
@@ -45,6 +44,22 @@ class PlayerSpec extends WordSpec with Matchers {
     }
     "return false if the move is not possible" in {
       player.set(0, 0) should be (false)
+    }
+  }
+  "checkMoves._2" should {
+    "be empty if there is no valid move for the tile" in {
+      player.checkMoves(0, 0)._2 shouldBe empty
+    }
+    "not be empty if there are valid moves for the tile" in {
+      player.checkMoves(4,3)._2 should not be empty
+    }
+  }
+  "checkRec" should {
+    "return a tuple with values between 0 and 7 if there is a valid move" in {
+      player.checkRec(3, 4, (1, 0)) should be (5, 4)
+    }
+    "return (-1, -1) if there is no valid move in this direction" in {
+      player.checkRec(0, 0, (-1, 0)) should be (-1, -1)
     }
   }
 }
