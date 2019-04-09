da12311e70d2d63aee70938713f89c1f7dfae93a
marcothuemmler <marco.thuemmler@me.com>
2019-04-09 22:55:03 +0200

    Preliminary draft for Text UI

--- src/main/scala/de/htwg/se/othello/Othello.scala
+++ src/main/scala/de/htwg/se/othello/Othello.scala
@@ -2,25 +2,50 @@ package de.htwg.se.othello
 
 import de.htwg.se.othello.model.{Board, Game, Player}
 
+import scala.io.StdIn
+import scala.util.Random
+
 object Othello {
 
   def main(args: Array[String]): Unit = {
-
     val game = Game(new Board)
-    val player1 = new Player("Player1", 1, game)
-    val player2 = new Player("Player2", 2, game)
-    player1.set(2, 3)
-    player2.set(2, 2)
-    player1.set(3, 2)
-    player2.set(2, 4)
-    player1.set(5, 5)
-    player2.set(5, 3)
-    player1.set(1, 4)
-    player2.set(2, 5)
-    player1.set(1, 2)
-    player2.set(0, 4)
-    player1.set(0, 5)
-    player2.set(0, 3)
-    player1.set(0, 2)
+    var input = ""
+    println("Please input name")
+    val player1 = new Player(StdIn.readLine(), 1, game)
+    val player2 = new Player("bot", 2, game)
+    val players = Array[Player](player1, player2)
+    var i = 0
+    var x: Int = 0
+    var y: Int = 0
+    while (input != "q") {
+      val currentPlayer = players(i)
+      if (i == 0) {
+        currentPlayer.highlight()
+        input = StdIn.readLine()
+        val tokens = input.split(" ")
+        x = tokens(0).toInt
+        y = tokens(1).toInt
+      } else {
+        val tile = getRandomFirst(currentPlayer.moves().filter(_._2.nonEmpty))
+        val move = getRandomSecond(tile._2)
+        x = move._1
+        y = move._2
+        Thread.sleep(1000)
+      }
+      if (currentPlayer.set(x, y)) {
+        if (i + 1 == players.length) i = 0 else i = 1
+        game.update()
+      }
+    }
+  }
+
+  def getRandomFirst(map: Map[(Int, Int), List[(Int, Int)]]): ((Int, Int), List[(Int, Int)]) = {
+    val r = Random
+    map.toList(r.nextInt(map.keys.size))
+  }
+
+  def getRandomSecond(list: List[(Int, Int)]): (Int, Int) = {
+    val r = Random
+    list(r.nextInt(list.size))
   }
 }
