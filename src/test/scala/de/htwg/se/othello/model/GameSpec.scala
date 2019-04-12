7273030cc009baf063b821821291294d295480a3
Yue Zhang <xyyxyyxyywww@gmail.com>
2019-04-12 06:56:50 +0200

    add class tests

---
+++ src/test/scala/de/htwg/se/othello/model/GameSpec.scala
@@ -0,0 +1,40 @@
+package de.htwg.se.othello.model
+
+import org.scalatest.{Matchers, WordSpec}
+
+class GameSpec extends WordSpec with Matchers {
+    val game = Game(new Board)
+    "flip" should {
+      "set the value of the cell to new value" in {
+        game.flip(7, 7, 2)
+        game.board.field(7)(7) should be(2)
+      }
+      "set the value of the cell vertically" in {
+        game.flip(5,4,1)
+        game.flip(3, 4, 1)
+        game.board.field(4)(4) should be (1)
+      }
+      "set the value of the cell horizontally " in{
+        game.flip(4,3,1)
+        game.flip(4,5,1)
+        game.board.field(4)(4) should be (1)
+      }
+      "set the value from up-left to down-right" in {
+        game.flip(2,2,1)
+        game.flip(5,5,1)
+        game.board.field(3)(3) should be (1)
+        game.board.field(4)(4) should be (1)
+      }
+      "set the value from up-right to down-left" in{
+        game.flip(2,5,2)
+        game.flip(5,2,2)
+        game.board.field(4)(3) should be (2)
+        game.board.field(3)(4) should be (2)
+      }
+
+
+
+
+
+    }
+}
