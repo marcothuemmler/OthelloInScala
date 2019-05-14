cc8204b22ca8a04b330a0415828471c32fcbe245
Yue Zhang <xyyxyyxyywww@gmail.com>
2019-05-15 00:01:29 +0200

    gamestatus and a few information of the game

--- src/main/scala/de/htwg/se/othello/controller/GameStatus.scala
+++ src/main/scala/de/htwg/se/othello/controller/GameStatus.scala
@@ -2,16 +2,20 @@ package de.htwg.se.othello.controller
 
 object GameStatus extends  Enumeration{
   type GameStatus = Value
-  val IDLE, NEWGAME, FINISHED = Value
+  val IDLE, NEWGAME, FINISHED, Set_Player1, Set_Bot = Value
 
   val map: Map[GameStatus, String] = Map[GameStatus, String](
     IDLE -> "",
     NEWGAME -> "A new game was created.",
-    FINISHED -> "Finished"
+    FINISHED -> "Finished",
+    Set_Player1 -> "Cell set by Player1",
+    Set_Bot -> "Cell set by Bot"
 
-
+    //give a suggestion
 
 
   )
+
+  def information(gamestatus: GameStatus) : String = map(gamestatus)
 
 }
