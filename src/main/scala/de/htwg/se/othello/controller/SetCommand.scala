26d1e2ab70aac915114e9ebf56154869f8e341b7
marcothuemmler <marco.thuemmler@me.com>
2019-05-16 14:15:51 +0200

    Simplified redo method

--- src/main/scala/de/htwg/se/othello/controller/SetCommand.scala
+++ src/main/scala/de/htwg/se/othello/controller/SetCommand.scala
@@ -26,11 +26,6 @@ class SetCommand(toSquare: (Int, Int), value: Int, controller: Controller) exten
 
   override def redoStep(): Unit = {
     controller.player = controller.nextPlayer
-    val legal = controller.moves.filter(o => o._2.contains(toSquare))
-    for {
-      fromSquare <- legal.keys
-    } controller.board = controller.board.flipLine(fromSquare, toSquare, value)
-    controller.board = controller.board.deHighlight
-    if (controller.board.gameOver) controller.gameStatus = GameStatus.GAME_OVER
+    controller.set(toSquare)
   }
 }
