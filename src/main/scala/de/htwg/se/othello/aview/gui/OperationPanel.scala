b6539543f7dae2700d41abbb876c7c299cdb928b
Yue Zhang <xyyxyyxyywww@gmail.com>
2019-06-23 00:46:50 +0200

    operation panel refactor

--- src/main/scala/de/htwg/se/othello/aview/gui/OperationPanel.scala
+++ src/main/scala/de/htwg/se/othello/aview/gui/OperationPanel.scala
@@ -1,17 +1,43 @@
 package de.htwg.se.othello.aview.gui
 
-import java.awt.{Color, Dimension}
+import java.awt.{Color, GridLayout, RenderingHints}
+
 
 import de.htwg.se.othello.controller.controllerComponent.ControllerInterface
+import javax.swing.ImageIcon
+import javax.swing.border.LineBorder
+import javax.imageio.ImageIO
 
-import scala.swing.FlowPanel
+import scala.concurrent.{ExecutionContext, Future}
+import scala.swing.event.MouseClicked
+import scala.swing.{BorderPanel, BoxPanel, Dimension, FlowPanel, Font, Graphics2D, GridPanel, Label, Orientation}
 
 class OperationPanel(controller: ControllerInterface, Hoehe: Int) extends  FlowPanel{
   val operationsides = 200
   background = Color.LIGHT_GRAY
   preferredSize = new Dimension(operationsides, Hoehe)
 
+  def presentPlayer: BoxPanel =  new BoxPanel(Orientation.Vertical){
 
+  }
+  def playerWhite: Label = new Label()  {
+    icon = new ImageIcon(getClass.getResource(s"resources/2.png"))
+    foreground = new Color(200, 200, 200)
+  }
+  def playerBlack: Label = new Label()  {
+    icon = new ImageIcon(getClass.getResource(s"resources/1.png"))
+    foreground = new Color(200, 200, 200)
+  }
+
+  def Undo: BoxPanel = new BoxPanel(Orientation.Horizontal) {
+      if (controller.isReady){
+        contents
+      }
+  }
+
+  def Redo: BoxPanel = new BoxPanel(Orientation.Horizontal) {
+
+  }
 
 
 
