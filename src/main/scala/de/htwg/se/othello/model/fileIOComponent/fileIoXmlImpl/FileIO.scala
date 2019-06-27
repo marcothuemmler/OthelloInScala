19b588ed3c06422a648f45a1c8366001f0792ae3
marcothuemmler <marco.thuemmler@me.com>
2019-06-27 22:46:41 +0200

    FileIO Xml implementation

--- src/main/scala/de/htwg/se/othello/model/fileIOComponent/fileIoXmlImpl/FileIO.scala
+++ src/main/scala/de/htwg/se/othello/model/fileIOComponent/fileIoXmlImpl/FileIO.scala
@@ -1,44 +1,35 @@
 package de.htwg.se.othello.model.fileIOComponent.fileIoXmlImpl
 
-import com.google.inject.Guice
-import com.google.inject.name.Names
-import de.htwg.se.othello.OthelloModule
 import de.htwg.se.othello.model.boardComponent.BoardInterface
 import de.htwg.se.othello.model.boardComponent.boardBaseImpl.Board
 import de.htwg.se.othello.model.fileIOComponent.FileIOInterface
 import scala.xml.{Elem, PrettyPrinter}
 
-class FileIO extends FileIOInterface{
-  override def load: Option[BoardInterface] = {
-    var boardOption: Option[BoardInterface] = None
+class FileIO extends FileIOInterface {
+  override def load: BoardInterface = {
+    var board: BoardInterface = null
     val file = scala.xml.XML.loadFile("grid.xml")
-    val sizeAttr = file \\ "grid" \ "@size"
+    val sizeAttr = file \\ "board" \ "@size"
     val size = sizeAttr.text.toInt
-    val injector = Guice.createInjector(new OthelloModule)
-
+    board = new Board(size)
     /**
-    size match {
-      case 1 => boardOption = Some(injector.instance[BoardInterface](Names.named("tiny")))
-      case 4 => boardOption = Some(injector.instance[BoardInterface](Names.named("small")))
-      case 8 => boardOption = Some(injector.instance[BoardInterface](Names.named("normal")))
-      case _ =>
-    }
-    **/
+     * size match {
+     * case 1 => board = Some(injector.instance[BoardInterface](Names.named("tiny")))
+     * case 4 => board = Some(injector.instance[BoardInterface](Names.named("small")))
+     * case 8 => board = Some(injector.instance[BoardInterface](Names.named("normal")))
+     * case _ =>
+     * }
+     */
     val cellNodes = file \\ "cell"
-    boardOption match {
-      case Some(board) =>
-        var _board = board
         for (cell <- cellNodes) {
           val row: Int = (cell \ "@row").text.toInt
           val col: Int = (cell \ "@col").text.toInt
           val value: Int = cell.text.trim.toInt
-          _board = _board.flip(col, row, value)
+          board = board.flip(row, col, value)
         }
-        boardOption = Some(_board)
-      case None =>
+
+    board
     }
-    boardOption
-  }
 
   def save(board: BoardInterface): Unit = saveString(board)
 
@@ -52,17 +43,18 @@ class FileIO extends FileIOInterface{
   }
 
   def boardToXml(board: BoardInterface): Elem = {
-    <board size={board.size.toString}>
-      {for {
-      row <- 0 until board.size
-      col <- 0 until board.size
-    } yield cellToXml(board, row, col)}
+    <board size={ board.size.toString }>
+      {
+      for {
+        row <- 0 until board.size
+        col <- 0 until board.size
+      } yield cellToXml(board, row, col) }
     </board>
   }
 
   def cellToXml(board: BoardInterface, row: Int, col: Int): Elem = {
-    <cell row={row.toString} col={col.toString}>
-      {board.valueOf(row, col)}
+    <cell row={ row.toString } col={ col.toString }>
+      { board.valueOf(row, col) }
     </cell>
   }
 }
