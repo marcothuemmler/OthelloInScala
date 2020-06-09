51c5d930bcee2daed063dc994dd6444050d212a5
marcothuemmler <marco.thuemmler@me.com>
2020-06-10 01:42:47 +0200

    Implement tests for Othello HTTP Server

--- src/main/scala/de/htwg/se/othello/Othello.scala
+++ src/main/scala/de/htwg/se/othello/Othello.scala
@@ -1,26 +1,38 @@
 package de.htwg.se.othello
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
 import com.google.inject.{Guice, Injector}
-import de.htwg.se.othello.aview.{HttpServer, Tui}
 import de.htwg.se.othello.aview.gui.SwingGui
+import de.htwg.se.othello.aview.{OthelloHttpService, Tui}
 import de.htwg.se.othello.controller.controllerComponent.ControllerInterface
 import net.codingwell.scalaguice.InjectorExtensions.ScalaInjector
 
+import scala.concurrent.Future
 import scala.io.StdIn.readLine
+
+class OthelloServer(implicit val system: ActorSystem, implicit val controller: ControllerInterface) extends OthelloHttpService {
+  def startServer(address: String, port: Int): Future[Http.ServerBinding] = {
+    Http().bindAndHandle(route, address, port)
+  }
+}
 
 object Othello {
 
-  val injector: Injector = Guice.createInjector(new OthelloModule)
-
-  val globalController: ControllerInterface = injector.instance[ControllerInterface]
-  val webServer = new HttpServer(globalController)
-  val tui = new Tui(globalController)
-  if (!sys.env.contains("DOCKER_ENV")) new SwingGui(globalController)
-
-  globalController.newGame
-
   def main(args: Array[String]): Unit = {
-    while (true) tui.processInputLine(readLine)
-    webServer.unbind()
+
+    val injector: Injector = Guice.createInjector(new OthelloModule)
+    implicit val globalController: ControllerInterface = injector.instance[ControllerInterface]
+    implicit val actorSystem: ActorSystem = ActorSystem("othello-server")
+    val webServer = new OthelloServer
+    val tui = new Tui(globalController)
+    if (!sys.env.contains("DOCKER_ENV")) new SwingGui(globalController)
+
+    webServer.startServer("0.0.0.0", 8080)
+    globalController.newGame
+
+    while (true) {
+      tui.processInputLine(readLine)
+    }
   }
 }
