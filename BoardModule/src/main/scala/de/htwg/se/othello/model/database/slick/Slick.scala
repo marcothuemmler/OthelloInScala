9618043a2c1b6b8343add8e2fbba223aa105d7c6
kevin-ba <kevin.bauer2@gmx.de>
2020-06-10 00:23:12 +0200

    dao and mysql

---
+++ BoardModule/src/main/scala/de/htwg/se/othello/model/database/slick/Slick.scala
@@ -0,0 +1,28 @@
+package de.htwg.se.othello.model.database.slick
+
+import de.htwg.se.othello.model.boardComponent.BoardInterface
+import de.htwg.se.othello.model.database.Dao
+import slick.jdbc.JdbcBackend.Database
+import slick.jdbc.MySQLProfile.api._
+
+case class Slick() extends Dao {
+
+  val db = Database.forURL(
+    url = "jdbc:mysql://localhost:3306",
+    driver = "com.mysql.cj.jdbc.Driver",
+    user = "root",
+    password = "othello"
+  )
+
+  val schema = TableQuery[TestTable].schema
+
+  db.run(DBIO.seq(schema.createIfNotExists))
+
+  override def read(board: BoardInterface): Unit = ???
+
+  override def create(board: BoardInterface): Unit = ???
+
+  override def update(board: BoardInterface): Unit = ???
+
+  override def delete(board: BoardInterface): Unit = ???
+}
--- build.sbt
+++ build.sbt
@@ -14,7 +14,11 @@ ThisBuild / libraryDependencies := Seq(
   "com.typesafe.akka" %% "akka-http" % "10.1.12",
   "com.typesafe.akka" %% "akka-stream" % "2.6.5",
   "com.typesafe.akka" %% "akka-stream-testkit" % "2.6.5",
-  "com.typesafe.akka" %% "akka-http-testkit" % "10.1.12"
+  "com.typesafe.akka" %% "akka-http-testkit" % "10.1.12",
+  "com.typesafe.slick" %% "slick" % "3.3.2",
+  "org.slf4j" % "slf4j-nop" % "1.7.30",
+  "com.typesafe.slick" %% "slick-hikaricp" % "3.3.2",
+  "mysql" % "mysql-connector-java" % "8.0.20"
 )
 
 lazy val OthelloMainModule = project.in(file("."))
