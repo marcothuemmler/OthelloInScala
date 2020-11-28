4d09c91202395929e000622a791066f10dd88eef
Thanh Son Dang <sondang3298_hp@yahoo.com.vn>
2020-11-28 01:47:18 +0100

    add events CellsChanged

--- src/main/scala/de/htwg/se/othello/controller/controllerComponent/controllerMockImpl/Event.scala
+++ src/main/scala/de/htwg/se/othello/controller/controllerComponent/controllerMockImpl/Event.scala
@@ -1,10 +1,14 @@
 package de.htwg.se.othello.controller.controllerComponent.controllerMockImpl
 
 import de.htwg.se.othello.controller.controllerComponent.GameStatus.{GameStatus, ILLEGAL}
+import play.api.libs.json.JsValue
 
 import scala.swing.event.Event
 
 class BoardChanged extends Event
+class BoardSizeChanged(op: String) extends Event
+class BoardHighlightChanged(currentPlayerValue: Int, moves: JsValue) extends Event
+class CellsChanged(cells: JsValue) extends Event
 class ModeChanged(playerCount: String) extends Event
 class CurrentPlayerChanged(to: Int) extends Event
 class DifficultyChanged(difficulty: String) extends Event
